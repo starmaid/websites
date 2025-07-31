147c2939d0939dcc367daa2d677aaaed2204925d
starmaid <nicholas.masso.14@gmail.com>
2025-07-30 23:52:20 -0700

    move the code into a timer thing

--- qr/script.js
+++ qr/script.js
@@ -1,15 +1,23 @@
-let r = new XMLHttpRequest();
-r.addEventListener('load', function () {
-  document.querySelector('#stats').innerText = JSON.parse(this.responseText).count
-})
+var t = setInterval(function () {
+  if (window.goatcounter && window.goatcounter.visit_count) {
+    clearInterval(t)
+    
+    let r = new XMLHttpRequest();
+    r.addEventListener('load', function () {
+      document.querySelector('#stats').innerText = JSON.parse(this.responseText).count
+    })
 
-// Should be exact (encoded) path as it appears in the UI. This uses
-// GoatCounter's count.js
-let path = window.goatcounter.get_data()['p']
+    // Should be exact (encoded) path as it appears in the UI. This uses
+    // GoatCounter's count.js
+    let path = window.goatcounter.get_data()['p']
 
-// Or alternatively, do it manually. Will return HTTP status 404 if the
-// path is not found).
-//let path = location.pathname
+    // Or alternatively, do it manually. Will return HTTP status 404 if the
+    // path is not found).
+    //let path = location.pathname
 
-r.open('GET', 'https://starmaid.goatcounter.com/counter/' + encodeURIComponent(path) + '.json')
-r.send()
+    r.open('GET', 'https://starmaid.goatcounter.com/counter/' + encodeURIComponent(path) + '.json')
+    r.send()
+  }
+}, 100)
+
+
